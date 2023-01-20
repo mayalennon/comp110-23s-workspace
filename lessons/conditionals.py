1e49d681d49b07b16af6a63bf1e24630d1407e9f
Maya Lennon <mlennon@unc.edu>
2023-01-20 14:57:29 -0500

    Progress on Exercies 1

---
+++ lessons/conditionals.py
@@ -0,0 +1,12 @@
+"""If it is raining, tell me to pack umbrella"""
+
+weather: str = input("What is the weather like? ")
+
+if(weather == "rain"):
+    print("Pack an umbrella!")
+    print("But splash in the puddles!")
+else:
+    if (weather == "snow"):
+        print("Pack a jacket!701030")
+    print("You don't need an umbrella!")
+print("Have a lovely day!")
